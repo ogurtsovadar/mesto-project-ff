c7d7415c7b7d19a0d83845461cf26e43bf12b33d
Daria Ogurtsova <ogurtsova.darr@yandex.ru>
2023-11-11 20:14:51 +0300

    Первое ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,30 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
+const content = document.querySelector('.content');
+const placesList = content.querySelector('.places__list');
 
 // @todo: Функция создания карточки
+function createCard(cardName, cardPic, callbcak) {
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+
+    cardElement.querySelector('.card__description').textContent = cardName;
+    cardElement.querySelector('.card__image').src = cardPic;
+
+    placesList.append(cardElement);
+
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+    deleteButton.addEventListener('click', deleteCard)
+};
 
 // @todo: Функция удаления карточки
 
+function deleteCard(evt) {
+    evt.target.parentElement.remove();
+  };
+
 // @todo: Вывести карточки на страницу
+initialCards.forEach(function (item) {
+    createCard(item.name, item.link, deleteCard);
+});
